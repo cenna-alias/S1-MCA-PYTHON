44f227c0c8be022bb44e2561ea0678837e95d782
Cenna Alias <cennaalias29@gmail.com>
2026-01-11 20:24:41 +0530

    first

--- pgm22-sumoflist.py
+++ pgm22-sumoflist.py
@@ -1,5 +1,4 @@
-# Sum all the items in a list.
-
-nums = [int(x) for x in input("Enter numbers separated by space:").split()]
+# Find the sum of all items in a list
+nums = [int(x) for x in input("Enter numbers separated by space: ").split()]
 total = sum(nums)
 print("Sum of all items:", total)
--- pgm25-numofchar.py
+++ pgm25-numofchar.py
@@ -1,5 +1,8 @@
-s = input("Enter a string:")
+# Count the number of characters (character frequency) in a string
+s = input("Enter a string: ")
+
 freq = {}
 for char in s:
-	freq[char] = freq.get(char,0)+1
-print("Character frequency:",freq)
+    freq[char] = freq.get(char, 0) + 1
+
+print("Character frequency:", freq)
